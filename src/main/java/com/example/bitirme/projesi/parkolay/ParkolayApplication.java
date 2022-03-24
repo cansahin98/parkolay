9ad4c440ee5b9a9ca5f3317285b1419fb28177f2
cansahin98 <70703577+cansahin98@users.noreply.github.com>
2022-03-24 21:30:54 +0300

    first commit

--- src/main/java/com/example/bitirme/projesi/parkolay/ParkolayApplication.java
+++ src/main/java/com/example/bitirme/projesi/parkolay/ParkolayApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("com.example.bitirme.projesi")
 public class ParkolayApplication  {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ParkolayApplication.class, args);
 	}
